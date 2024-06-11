c8bf7aa3b0bd6cea81f44d73b1ae73a1ed2756a0
Ajay <ajaykrishna13@gmail.com>
2024-06-11 12:08:52 +0530

    modified:   practice.js

--- practice.js
+++ practice.js
@@ -366,7 +366,7 @@
 function selectionSort(arr){
     for(let i=0;i<arr.length;i++){
         let minIndex=i
-        for(let j=i+1;j<arr.length;j++){
+        for(let j=i+1  ;j<arr.length;j++){
             if(arr[j]<arr[minIndex]){
                 minIndex=j
             }
